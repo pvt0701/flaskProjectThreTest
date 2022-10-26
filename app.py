cfc750cfcb48791d2ae8ff8ef3899de502eaedee
Pavlo Tkachenko <pvt0701@gmail.com>
2022-10-26 18:19:56 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,65 @@
+from flask import Flask, render_template, redirect, url_for, request
+from flask_sqlalchemy import SQLAlchemy
+import os
+from flask_migrate import Migrate
+from dotenv import load_dotenv
+
+
+app = Flask(__name__)
+load_dotenv()
+
+app.config['SQLALCHEMY_DATABASE_URI'] = (
+    f'postgresql+psycopg2://{os.getenv("POSTGRES_USER")}:' +
+    f'{os.getenv("POSTGRES_PW")}@' +
+    f'{os.getenv("POSTGRES_HOST")}/' +
+    f'{os.getenv("POSTGRES_DB")}'
+)
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(1024), nullable=False)
+
+    def __init__(self, text, tags):
+        self.text = text.strip()
+        self.tags = [
+            Tag(text=tag.strip()) for tag in tags.split(',')
+        ]
+
+
+class Tag(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(32), nullable=False)
+
+    message_id = db.Column(db.Integer, db.ForeignKey('message.id'), nullable=False)
+    message = db.relationship('Message', backref=db.backref('tags', lazy=True))
+
+
+@app.route('/', methods=['GET'])
+def hello_world():
+    return render_template('index.html')
+
+
+@app.route('/main', methods=['GET'])
+def main():
+    return render_template('main.html', messages=Message.query.all())
+
+
+@app.route('/add_message', methods=['POST'])
+def add_message():
+    text = request.form['text']
+    tag = request.form['tag']
+
+    db.session.add(Message(text, tag))
+    db.session.commit()
+
+    return redirect(url_for('main'))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
