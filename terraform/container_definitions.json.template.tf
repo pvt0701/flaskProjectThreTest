038709eac712f2fbeac63435c8d205c6a6eedd96
Pavlo Tkachenko <pvt0701@gmail.com>
2023-01-03 14:04:35 +0200

    reload 2

--- terraform/container_definitions.json.template.tf
+++ terraform/container_definitions.json.template.tf
@@ -4,10 +4,10 @@ data "template_file" "container_definitions" {
 
   vars = {
 #    account_id = data.aws_ssm_parameter.container_definitions__account_id.value
-    POSTGRES_HOST = var.POSTGRES_HOST
-    POSTGRES_DB = var.POSTGRES_DB
-    POSTGRES_PW = var.POSTGRES_PW
-    POSTGRES_USER = var.POSTGRES_USER
+#    POSTGRES_HOST = var.POSTGRES_HOST
+#    POSTGRES_DB = var.POSTGRES_DB
+#    POSTGRES_PW = var.POSTGRES_PW
+#    POSTGRES_USER = var.POSTGRES_USER
     account_id = data.aws_ssm_parameter.container_definitions__account_id.value
   }
   depends_on = [aws_ssm_parameter.container_definitions__account_id]
